7dc677a6482ef98e5b82d8cba608b08ad9cf4432
JuanDanielG <jdgaleanon@ufpso.edu.co>
2024-10-24 18:03:59 -0500

    segundo commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './publico/pages/home/home.component';
+import { ProductosComponent } from './administrador/productos/pages/productos/productos.component';
+import { UsuarioComponent } from './administrador/usuarios/pages/usuario/usuario.component';
+import { ErrorComponent } from './publico/pages/error/error.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: ' ', // significa que apenas ingreso no hay nada en la url
+        redirectTo: 'home',
+        pathMatch: "full"
+        //component : HomeComponent // lo que hay en el component lo pega en la pagina
+
+    },
+    {
+        path: 'home', // ruta de home
+        component: HomeComponent
+    },
+    {
+        path: 'administrador/producto', // ruta administrador producto
+        component : ProductosComponent
+    },
+    {
+        path: "administrador/usuario",  //ruta administrador usuario
+        component : UsuarioComponent
+    },
+    {
+        path: "**",
+        component: ErrorComponent
+    }
+];
