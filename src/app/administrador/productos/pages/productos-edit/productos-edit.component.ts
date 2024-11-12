863e22debbb96adf0376da5cc49897d602d3076d
Jhonatan Quiroga <jdquirogape@ufpso.edu.co>
2024-11-12 15:58:49 -0500

    Editar Producto

--- src/app/administrador/productos/pages/productos-edit/productos-edit.component.ts
+++ src/app/administrador/productos/pages/productos-edit/productos-edit.component.ts
@@ -1,12 +1,115 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ProductoService } from '../../../../services/producto.service';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, Router, RouterLink, RouterModule } from '@angular/router';
+import { switchMap } from 'rxjs';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-productos-edit',
-  standalone: true,
-  imports: [],
+  standalone:true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    RouterModule,
+    RouterLink,
+  ],
   templateUrl: './productos-edit.component.html',
-  styleUrl: './productos-edit.component.css'
+  styleUrls: ['./productos-edit.component.scss']
 })
-export class ProductosEditComponent {
+export class ProductosEditComponent implements OnInit {
+  editForm: FormGroup;
+  productId!: number;
+  protected readonly localStorage = localStorage;
 
-}
+  parsedImages: any = [];
+
+  constructor(
+    private fb: FormBuilder,
+    private productoService: ProductoService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+      this.editForm = this.fb.group({
+        title: ['', Validators.required],
+        price: ['', [Validators.required, Validators.pattern("^[0-9]*$")]],
+        description: ['', Validators.required],
+        images: ['', Validators.required],
+        categoryId: ['', Validators.required]
+      });
+     }
+
+     ngOnInit(): void {
+      this.route.paramMap.subscribe(params => {
+        this.productId = Number(params.get('id'));
+        if (this.productId) {
+          this.loadProductData();
+        }
+      });
+    }
+
+    loadProductData(): void {
+      this.productoService.getProductById(this.productId).subscribe({
+          next: (data) => {
+            this.parsedImages = data.images;
+            if (data.images[0].startsWith('["')) {
+              this.parsedImages = JSON.parse(data.images);
+            }
+            this.editForm.patchValue(data)
+            this.parsedImages = JSON.parse(data.images);
+          }
+        }
+      );
+    }
+
+    getUserName(): string {
+      const username = localStorage.getItem('name_user');
+      if (username) {
+        return username.split('@')[0];
+      }
+      return 'Usuario';
+    }
+
+    logOut(): void {
+      localStorage.clear();
+      this.router.navigate(['home']);
+    }
+
+    onSubmit(): void {
+      if (this.editForm.valid) {
+        const productData = {
+          title: this.editForm.value.title,
+          price: this.editForm.value.price,
+          description: this.editForm.value.description,
+          categoryId: this.editForm.value.categoryId,
+          images: [this.editForm.value.images],
+        }
+        this.productoService.updateProducto(this.productId, productData).subscribe(
+          (response) => {
+            Swal.fire({
+              title: 'Producto actualizado exitosamente',
+              icon: 'success',
+              confirmButtonText: 'OK'
+            }).then(() => {
+              this.router.navigate(['/producto']);
+            });
+          },
+          (error) => {
+            console.error('Error updating product', error);
+            Swal.fire({
+              title: 'Error actualizando producto',
+              text: 'Hubo un problema al actualizar el producto. Por favor, inténtalo de nuevo.',
+              icon: 'error'
+            });
+          }
+        );
+      } else {
+        Swal.fire({
+          title: 'Error al actualizar producto',
+          text: 'Los campos no pueden estar vacíos o pueden estar incorrectos',
+          icon: 'error'
+        });
+      }
+    }
+  }
